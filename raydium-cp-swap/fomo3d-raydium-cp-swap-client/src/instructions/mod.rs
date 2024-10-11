f182deee919111392b89ed94a039e7980882db7b
stacc <jarettrsdunn+git@gmail.com>
2024-10-11 17:10:35 +0100

    naughty ray

--- raydium-cp-swap/fomo3d-raydium-cp-swap-client/src/instructions/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod amm_instructions;
-pub mod events_instruction_parse;
-pub mod rpc;
-pub mod token_instructions;
-pub mod utils;
--- raydium-cp-swap/programs/cp-swap/src/states/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod config;
-pub use config::*;
-
-pub mod pool;
-pub use pool::*;
-pub mod events;
-pub use events::*;
-pub mod oracle;
-pub use oracle::*;
