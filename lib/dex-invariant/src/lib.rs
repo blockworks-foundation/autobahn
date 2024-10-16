a1f24f6f9cbe7a8896716a37238d16e24b4d0753
none00y <mich_smyk@wp.pl>
2024-10-16 17:22:20 +0200

    use mainnet address

--- lib/dex-invariant/src/lib.rs
+++ lib/dex-invariant/src/lib.rs
@@ -7,11 +7,4 @@ pub use invariant_dex::InvariantDex;
 
 use solana_sdk::declare_id;
 
-// declare_id!("HyaB3W9q6XdA5xwpU4XnSZV94htfmbmqJXZcEbRaJutt");
-declare_id!("D8Xd5VFXJeANivc4LXEzYqiE8q2CGVbjym5JiynPCP6J");
-
-mod authority {
-    use solana_sdk::declare_id;
-
-    declare_id!("5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1");
-}
+declare_id!("HyaB3W9q6XdA5xwpU4XnSZV94htfmbmqJXZcEbRaJutt");
