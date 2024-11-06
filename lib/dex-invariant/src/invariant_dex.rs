4e699e6546888c48c5320c343fe114b045e9971d
none00y <mich_smyk@wp.pl>
2024-11-06 19:19:56 +0100

    fix invariant token 2022

--- lib/dex-invariant/src/invariant_dex.rs
+++ lib/dex-invariant/src/invariant_dex.rs
@@ -4,10 +4,7 @@ use std::{
 };
 
 use anchor_lang::{AnchorDeserialize, Id};
-use anchor_spl::{
-    token::spl_token::{self, state::AccountState},
-    token_2022::Token2022,
-};
+use anchor_spl::{token::spl_token::{self, state::AccountState}, token_2022::Token2022};
 use anyhow::{Context, Ok};
 use async_trait::async_trait;
 use invariant_types::{
@@ -246,8 +243,7 @@ impl DexInterface for InvariantDex {
         let banned_reserves = vaults
             .iter()
             .filter(|(_, reserve)| {
-                reserve.owner == Token2022::id()
-                    || spl_token::state::Account::unpack(reserve.data())
+                        spl_token::state::Account::unpack(reserve.data())
                         .unwrap()
                         .state
                         == AccountState::Frozen
